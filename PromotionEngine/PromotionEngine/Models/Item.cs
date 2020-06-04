9ddcffbbada36e84fc4c9ae4a45defc8a4a201d1
kodurinandini <koduri.nandini@gmail.com>
2020-06-04 21:39:36 +0530

    created models for items and Promos

---
+++ PromotionEngine/PromotionEngine/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Models
+{
+  public class Item
+  {
+    public string SKUId { get; set; } 
+    public string ItemName { get; set; }
+    public decimal UnitPrice { get; set; }
+  }
+}
---
+++ PromotionEngine/PromotionEngine/Models/Promotion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Models
+{
+  public class Promotion
+  {
+    public string PromotionId { get; set; }
+    public bool IsActive { get; set; }
+    public List<Item> ItemsInPromo { get; set; }
+    public char Operator { get; set; }
+    public decimal PromoPrice { get; set; }
+  }
+}
